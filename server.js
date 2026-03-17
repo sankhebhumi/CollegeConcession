437341ad6056ec4f494214b7541c9b2462208b12
Bhumi Naresh Sankhe <bhumi@example.com>
2026-03-17 08:33:42 +0530

    Configure for deployment (dynamic port & start script)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
